6d675089d1a114d91faeb375fa4d46dbf77c1242
kneazle <kneazle@yandex.ru>
2021-11-28 22:03:41 +0300

    feat: check uuid

--- index.js
+++ index.js
@@ -7,18 +7,38 @@ const port = 8000;
 const persons = [];
 
 const requestListener = function (req, res) {
-  res.setHeader('Content-Type', 'application/json');
+  //res.setHeader('Content-Type', 'application/json');
+  let parseUrl = req.url.split('/');
+
   switch (req.method) {
     case 'GET':
-      switch (req.url) {
-        case '/person':
-          res.statusCode = 200;
-          res.setHeader("Content-Type", "application/json");
-          res.end(JSON.stringify(persons));
-          break;
-        default:
-          res.statusCode = 404;
-          res.end(JSON.stringify({ error: 'Resource not found' }));
+      if (parseUrl[1] === 'person' && parseUrl[2]) {
+        if (/^[0-9a-fA-F]{8}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{12}$/.test(parseUrl[2])) {
+          let p = findPerson(parseUrl[2]);
+          if (p) {
+            res.statusCode = 200;
+            res.setHeader("Content-Type", "application/json");
+            res.end(JSON.stringify(p));
+          }
+          else {
+            res.statusCode = 404;
+            res.end(JSON.stringify({ error: 'Person not found' }));
+          }
+        }
+        else {
+          res.statusCode = 400;
+          res.end(JSON.stringify({ error: 'It is not valid UUID' }));
+        }
+      }
+      else if (parseUrl[1] === 'person') {
+        res.statusCode = 200;
+        res.setHeader("Content-Type", "application/json");
+        res.end(JSON.stringify(persons));
+        break;
+      }
+      else {
+        res.statusCode = 404;
+        res.end(JSON.stringify({ error: 'Resource not found' }));
       }
       break;
     case 'POST':
@@ -49,6 +69,31 @@ const requestListener = function (req, res) {
       break;
 
     case 'PUT':
+      let data = '';
+
+      req.on('data', (chunk) => {
+        data += chunk.toString();
+      });
+
+      req.on('end', () => {
+        data = JSON.parse(data);
+        /* 
+                if (checkProperties(data)) {
+                  let uuid = crypto.randomUUID();
+                  data.id = uuid;
+        
+                  persons.push(data);
+                  res.statusCode = 201;
+                  res.setHeader("Content-Type", "application/json");
+                  res.end(JSON.stringify(persons));
+                }
+                else {
+                  res.statusCode = 400;
+                  res.setHeader("Content-Type", "application/json");
+                  res.end(JSON.stringify({ error: 'name, age and hobbies are required fields' }));
+                } */
+      });
+
       break;
 
     case 'DELETE':
@@ -74,4 +119,12 @@ function checkProperties(data) {
     return false;
 
   return true;
+}
+
+function findPerson(id) {
+  for (let i = 0; i < persons.length; i++) {
+    if (persons[i].id === id)
+      return persons[i];
+  }
+  return false;
 }
